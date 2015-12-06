4727fb6ac2e58096f9fbd8c71d6b6627532c8599
Kevin Dungs <kevin@dun.gs>
2015-12-06 11:51:01 +0100

    Solve second part of day 2.
    Also serious refactoring.

--- src/02.cc
+++ src/02.cc
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <array>
 #include <iostream>
 #include <numeric>
 #include <vector>
@@ -8,13 +9,30 @@
 
 #include "helpers.h"
 
-unsigned calculate_material(unsigned l, unsigned w, unsigned h) {
-  auto sides = std::vector<unsigned>{l * w, w * h, h * l};
+struct Box {
+  std::array<unsigned, 3> sides;
+};
+
+unsigned calculate_paper(const Box& box) {
+  auto sides = ranges::view::zip_with(
+                   std::multiplies<unsigned>{}, box.sides,
+                   box.sides | ranges::view::cycle | ranges::view::drop(1))
+             | ranges::to_vector;
 
   return *std::min_element(std::begin(sides), std::end(sides)) +
          std::accumulate(
              std::begin(sides), std::end(sides), 0u,
              [](const auto& acc, const auto& x) { return acc + 2 * x; });
+}
+
+unsigned calculate_ribbon(const Box& box) {
+  auto sorted_sides = box.sides;
+  sorted_sides |= ranges::action::sort;
+  auto ribbon_box = 2 * (sorted_sides[0] + sorted_sides[1]);
+  auto ribbon_bow =
+      ranges::accumulate(box.sides, 1u, std::multiplies<unsigned>{});
+
+  return ribbon_box + ribbon_bow;
 }
 
 
@@ -27,14 +45,17 @@ int main(int argc, char* argv[]) {
   auto data = helpers::read_file(argv[1]);
   auto view_line = view::split('x')
                  | view::transform([](std::string s) { return std::stoul(s); }); 
-  auto split = data
+  auto boxes = data
              | view::split('\n')
              | view::transform(view_line)
              | view::transform([](auto rng) {
-                 auto xs = rng | to_vector;
-                 return calculate_material(xs[0], xs[1], xs[2]);
+                 auto b = Box{};
+                 copy(rng | view::take(3), std::begin(b.sides));
+                 return b;
                });
-  auto total = accumulate(split, 0u);
+  auto paper = accumulate(boxes | view::transform(calculate_paper), 0u);
+  auto ribbon = accumulate(boxes | view::transform(calculate_ribbon), 0u);
   
-  std::cout << total << '\n';
+  std::cout << "Paper: " << paper << " square feet\n";
+  std::cout << "Ribbon: " << ribbon << " feet \n";
 }
