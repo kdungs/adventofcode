2b60592f602c189a057a4a659b7b1b7331883e59
Kevin Dungs <kevin@dun.gs>
2016-12-09 16:23:06 +0100

    Solve both halves of day 9.
    Please tell me this is a smart solution :D

---
+++ 09/09.hs
@@ -0,0 +1,41 @@
+import Text.ParserCombinators.Parsec
+
+int :: Parser Int
+int = read <$> many1 digit
+
+data Exp = Txt String | Rep Int Int [Exp] deriving Show
+
+exps :: Parser [Exp]
+exps = many (txt <|> rep)
+
+txt :: Parser Exp
+txt = Txt <$> many1 upper
+
+rep :: Parser Exp
+rep = do
+  char '('
+  n <- int
+  char 'x'
+  r <- int
+  char ')'
+  s <- count n anyChar
+  let es = parse exps "" s
+  case es of
+    (Right es) -> return (Rep n r es)
+    (Left _) -> fail "Could not parse subexpr."
+  
+expLength1 :: Exp -> Int
+expLength1 e = case e of
+  (Txt s) -> length s
+  (Rep n r _) -> n * r
+  
+expLength2 :: Exp -> Int
+expLength2 e = case e of
+  (Txt s) -> length s
+  (Rep _ r se) -> r * (sum . map expLength2 $ se)
+  
+main = do
+  input <- getLine
+  let es = parse exps "" input
+  putStrLn . show $ sum . map expLength1 <$> es
+  putStrLn . show $ sum . map expLength2 <$> es
