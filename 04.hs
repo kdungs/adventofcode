09a5e39174b90f2591017a63f8928c316f0cf395
Kevin Dungs <kevin@dun.gs>
2017-12-04 16:41:31 +0100

    Solve day 4 in Haskell.

---
+++ 04.hs
@@ -0,0 +1,23 @@
+import Data.List (sort)
+import Data.Set (fromList, size)
+import Data.String (lines, words)
+
+-- Part 1.
+isGoodPassword :: String -> Bool
+isGoodPassword pw = (length ws) == (size (fromList ws)) where ws = words pw
+
+-- Part 2.
+isGoodPassword' :: String -> Bool
+isGoodPassword' pw = (length ws) == (size (fromList (map sort ws)))
+  where ws = words pw
+
+countGoodPasswords :: (String -> Bool) -> [String] -> Int
+countGoodPasswords f pws = length (filter f pws)
+
+
+main :: IO ()
+main = do
+  input <- readFile "input/04.txt"
+  let pws = lines input
+  putStrLn . show $ countGoodPasswords isGoodPassword pws
+  putStrLn . show $ countGoodPasswords isGoodPassword' pws
