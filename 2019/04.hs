739d8f920ff105c1788de437ba36fdb63df2366c
Kevin Dungs <kevin@dun.gs>
2019-12-04 09:25:17 +0100

    Solve day 4.

---
+++ 2019/04.hs
@@ -0,0 +1,52 @@
+import Text.ParserCombinators.Parsec
+import qualified Data.List as List
+
+digits :: Int -> [Int]
+digits i = List.reverse (rdigits i)
+  where rdigits 0 = []
+        rdigits x = (x `mod` 10):(rdigits (x `div` 10))
+
+-- Part 1
+groupLengths :: [Int] -> [Int]
+groupLengths xs = List.map List.length (List.group xs)
+
+hasMatchingAdjacentDigits :: [Int] -> Bool
+hasMatchingAdjacentDigits ds = List.any (>= 2) (groupLengths ds)
+
+isMonotonous :: [Int] -> Bool
+isMonotonous (x:y:rest) = (x <= y) && (isMonotonous (y:rest))
+isMonotonous _ = True
+
+isCandidate :: Int -> Bool
+isCandidate i = (List.length ds == 6)
+             && (hasMatchingAdjacentDigits ds)
+             && (isMonotonous ds)
+  where ds = digits i
+
+countInRange :: (Int -> Bool) -> (Int, Int) -> Int
+countInRange pred (left, right) = List.sum (List.map (fromEnum . pred) [left..right])
+
+-- Part 2
+hasTwoMatchingAdjacentDigits :: [Int] -> Bool
+hasTwoMatchingAdjacentDigits ds = List.any (== 2) (groupLengths ds)
+
+isCandidate2 :: Int -> Bool
+isCandidate2 i = (isCandidate i) && (hasTwoMatchingAdjacentDigits (digits i))
+
+-- Parser
+int :: Parser Int
+int = read <$> many1 digit
+
+range :: Parser (Int, Int)
+range = do
+  left <- int
+  char '-'
+  right <- int
+  pure (left, right)
+
+main :: IO ()
+main = do
+  contents <- getContents
+  let r = parse range "" contents
+  putStrLn . show $ (countInRange isCandidate) <$> r
+  putStrLn . show $ (countInRange isCandidate2) <$> r
