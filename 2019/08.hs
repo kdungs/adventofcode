eaaccf858b8568ebae4fbdf2fcd5b8052785022c
Kevin Dungs <kevin@dun.gs>
2019-12-08 11:46:17 +0100

    Refactor day 8...

--- 2019/08.hs
+++ 2019/08.hs
@@ -1,58 +1,75 @@
-import qualified Data.List as List
-import Text.ParserCombinators.Parsec
+import           Data.Functor                  (($>))
+import qualified Data.List                     as List
+import           Text.ParserCombinators.Parsec
 
-type Color = Char
-type Layer = [Color]
+-- List helpers
+groups :: Int -> [a] -> [[a]]
+groups _ []    = []
+groups size xs = List.take size xs : groups size (List.drop size xs)
 
-makeLayers :: (Int, Int) -> [Color] -> [Layer]
-makeLayers _ [] = []
-makeLayers (w, h) cs = List.take n cs : makeLayers (w, h) (List.drop n cs)
-  where n = w * h
+countElem :: Eq a => a -> [a] -> Int
+countElem x xs = List.length (List.filter (== x) xs)
 
-countDigits :: Char -> Layer -> Int
-countDigits d = List.length . List.filter (== d)
+-- Solution
+data Color
+  = Black
+  | White
+  | Transparent
+  deriving (Eq)
 
-layerWithMinimumZeroes :: [Layer] -> Layer
-layerWithMinimumZeroes ls = fst (List.minimumBy (\lhs rhs -> compare (snd lhs) (snd rhs)) zeroCounts)
-  where zeroCounts = List.map (\l -> (l, countDigits '0' l)) ls
+instance Show Color where
+  show Black       = "█"
+  show White       = " "
+  show Transparent = "X"
 
-part1 :: [Layer] -> Int
-part1 ls = (countDigits '1' lmin0) * (countDigits '2' lmin0)
-  where lmin0 = layerWithMinimumZeroes ls
+overlayC :: Color -> Color -> Color
+overlayC Transparent c = c
+overlayC c _           = c
 
-overlayColors :: Color -> Color -> Color
-overlayColors top btn
-  | top == '2' = btn
-  | otherwise = top
+data Image =
+  Image
+    { width  :: Int
+    , height :: Int
+    , layers :: [[Color]]
+    }
 
-overlay :: Layer -> Layer -> Layer
-overlay = List.zipWith overlayColors
+instance Show Image where
+  show (Image w _ ls) = List.intercalate "\n" (fmtRow <$> rows)
+    where
+      reduced = foldl1 (zipWith overlayC) ls
+      rows = groups w reduced
+      fmtRow cs = List.intercalate "" (show <$> cs)
 
-part2 :: [Layer] -> Layer
-part2 = foldl1 overlay
+fromList :: Int -> Int -> [Color] -> Image
+fromList w h cs = Image w h layers
+  where
+    layers = groups (w * h) cs
 
-formatImage :: (Int, Int) -> Layer -> [Char]
-formatImage (w, h) l = List.intercalate "\n" ((group w) (List.map formatDigit l))
-  where formatDigit d = if d == '0' then '█' else ' '
-        group _ [] = []
-        group w cs = List.take w cs : group w (List.drop w cs)
+layerWithMinimumBlack :: Image -> [Color]
+layerWithMinimumBlack (Image _ _ ls) = fst (List.minimumBy cmpSnd blackCounts)
+  where
+    blackCounts = (\l -> (l, countElem Black l)) <$> ls
+    cmpSnd lhs rhs = compare (snd lhs) (snd rhs)
 
 -- Parser
 color :: Parser Color
-color = digit
+color = choice [char '0' $> Black, char '1' $> White, char '2' $> Transparent]
 
 colors :: Parser [Color]
 colors = many1 color
 
 -- Main
+rightOrFail :: Show a => Either a b -> b
+rightOrFail (Left err) = error (show err)
+rightOrFail (Right x)  = x
+
 main :: IO ()
 main = do
   contents <- getContents
-  let cs = parse colors "" contents
-  let ls = makeLayers (25, 6) <$> cs
-  let p1 = part1 <$> ls
-  print p1
-  let p2 = part2 <$> ls
-  case p2 of
-    Left err -> print err
-    Right res -> putStrLn $ formatImage (25, 6) res
+  let cs = rightOrFail (parse colors "" contents)
+  let img = fromList 25 6 cs
+  print img
+  let lmb = layerWithMinimumBlack img
+  let cWhite = countElem White lmb
+  let cTransparent = countElem Transparent lmb
+  print (cWhite * cTransparent)
