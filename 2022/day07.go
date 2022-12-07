b926c1d02671c2be1d0835255415b38509298851
Kevin Dungs <kevin@dun.gs>
2022-12-07 23:55:35 +0100

    Solve day 7.

--- 2022/day07.go
+++ 2022/day07.go
@@ -10,19 +10,22 @@ import (
 )
 
 type File struct {
+	Name   string
 	Parent *Dir
 	Size   int
 }
 
 type Dir struct {
+	Name   string
 	Parent *Dir
 	Dirs   map[string]*Dir
 	Files  map[string]*File
 	size   *int
 }
 
-func NewDir(parent *Dir) *Dir {
+func NewDir(name string, parent *Dir) *Dir {
 	return &Dir{
+		Name:   name,
 		Parent: parent,
 		Dirs:   make(map[string]*Dir),
 		Files:  make(map[string]*File),
@@ -31,6 +34,7 @@ func NewDir(parent *Dir) *Dir {
 
 func (d *Dir) AddFile(name string, size int) *File {
 	f := &File{
+		Name:   name,
 		Parent: d,
 		Size:   size,
 	}
@@ -39,14 +43,41 @@ func (d *Dir) AddFile(name string, size int) *File {
 }
 
 func (d *Dir) AddDir(name string) *Dir {
-	dir := NewDir(d)
+	dir := NewDir(name, d)
 	d.Dirs[name] = dir
 	return dir
 }
 
+func (d *Dir) Size() int {
+	if d.size != nil {
+		return *d.size
+	}
+	size := 0
+	for _, file := range d.Files {
+		size += file.Size
+	}
+	for _, dir := range d.Dirs {
+		size += dir.Size()
+	}
+	d.size = &size
+	return size
+}
+
+func (d *Dir) Walk(fn func(*Dir)) {
+	queue := []*Dir{d}
+	for len(queue) > 0 {
+		cur := queue[0]
+		queue = queue[1:]
+		fn(cur)
+		for _, dir := range cur.Dirs {
+			queue = append(queue, dir)
+		}
+	}
+}
+
 func run() error {
 	scanner := bufio.NewScanner(os.Stdin)
-	root := NewDir(nil)
+	root := NewDir("", nil)
 	pwd := root
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -80,7 +111,31 @@ func run() error {
 			}
 		}
 	}
-	fmt.Println(root)
+
+	// Part 1
+	total1 := 0
+	root.Walk(func(d *Dir) {
+		size := d.Size()
+		if size <= 100000 {
+			total1 += size
+		}
+	})
+	fmt.Printf("Part 1: %d\n", total1)
+
+	// Part 2
+	all := 70000000
+	need := 30000000
+	used := root.Size()
+	free := all - used
+	min := used
+	root.Walk(func(d *Dir) {
+		size := d.Size()
+		freeWithout := free + size
+		if freeWithout >= need && size < min {
+			min = size
+		}
+	})
+	fmt.Printf("Part 2: %d\n", min)
 	return nil
 }
 
